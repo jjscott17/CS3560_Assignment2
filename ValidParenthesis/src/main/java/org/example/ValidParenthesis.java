b6f330a40d9a5343a225bd43df7b342d0f215016
Joseph Scott <165531069+jjscott17@users.noreply.github.com>
2025-03-04 22:47:24 -0800

    Update ValidParenthesis.java with comments

--- ValidParenthesis/src/main/java/org/example/ValidParenthesis.java
+++ ValidParenthesis/src/main/java/org/example/ValidParenthesis.java
@@ -3,19 +3,22 @@ package org.example;
 import java.util.*;
 
 public class ValidParenthesis {
-
+        // check if provided string is valid
         public static boolean isValid(String s) {
             Stack<Character> stack = new Stack<>();
             for(char c : s.toCharArray()) {
+                //for each openin bracket, a closing bracket is expected
                 if(c=='(') {
                     stack.push(')');
                 } else if (c=='{') {
                     stack.push('}');
                 }else if (c=='[') {
                     stack.push(']');
+                // return false if the stack is empty or incorrect characters are used
                 } else if (stack.isEmpty() || stack.pop() != c)
                     return false;
             }
-            return stack.isEmpty();
+        // once completed, return stack is empty
+        return stack.isEmpty();
         }
 }
